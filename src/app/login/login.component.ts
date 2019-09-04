5e00cab7398163ed218ec1c1801e1822ac995051
mrinal purohit <mrinal@mrinallabs.com>
2019-09-04 12:38:29 -0400

    adding on submit callback placeholder

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,11 +9,18 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   loginForm;
+  onSubmit;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, snackBar: MatSnackBar) {
     this.loginForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
+    this.onSubmit = () => {
+      if (this.loginForm.status === 'VALID') {
+        // hit login API here
+        snackBar.open('Invalid Credentials!', 'Dismiss');
+      }
+    };
   }
 }
